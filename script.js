5ea1c7133de1fc9e709478a683c198a3fe06a8b9
Jay7cvc <120598188+Jay7cvc@users.noreply.github.com>
2024-06-12 17:18:15 +0300

    script.js

--- script.js
+++ script.js
@@ -14,3 +14,40 @@ openPopup.addEventListener('click', function() {
 closePopup.addEventListener('click', function() {
   popup.style.display = 'none';
 });
+
+document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  let formData = new FormData(event.target);
+  let formDataObj = Object.fromEntries(formData.entries());
+
+  // Здесь можно добавить код для отправки данных на сервер или другую логику обратной связи
+  console.log(formDataObj);
+  
+  event.target.reset();
+});
+
+const images = document.querySelectorAll('.carousel img');
+  let currentIndex = 0;
+
+  function showImage(index) {
+    images.forEach((image, i) => {
+      if (i === index) {
+        image.classList.add('active');
+      } else {
+        image.classList.remove('active');
+      }
+    });
+  }
+
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  }
+
+  function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+  }
+
+  setInterval(nextImage, 3000);
